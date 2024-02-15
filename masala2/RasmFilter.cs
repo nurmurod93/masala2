20649e4377ec41abd5f7e611c671693920a8060a
Humoyun Ochilov Ravshan o'g'li <43071439+dotNet-developer1995@users.noreply.github.com>
2024-02-15 12:02:26 +0500

    Add project files.

---
+++ masala2/RasmFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala2
+{
+    public class RasmFilter
+    {
+        public void Yorqinlik(Rasm rasm)
+        {
+            Console.WriteLine("Yorqinlashtiruvchi.");
+        }
+        public void Toqlashtiruvchi(Rasm rasm)
+        {
+            Console.WriteLine("To'qlashtiruvchi.");
+        }
+        public void Olchami(Rasm rasm)
+        {
+            Console.WriteLine("O'lcham.");
+        }
+    }
+}
---
+++ masala2/RasmProsessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala2
+{
+    public class RasmProsessor
+    {
+        public delegate void RasmFilterHandler(Rasm rasm);
+        public void Jarayon(string path, RasmFilterHandler filterHandler)
+        {
+            var rasm = Rasm.Load(path);
+            filterHandler(rasm);
+        }
+    }
+}
